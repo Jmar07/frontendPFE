baa487557bbe3ce0a353d78f03785163f4d152d7
chouikh <chm3sa@gmail.com>
2022-04-26 04:35:20 +0100

    ++

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,6 @@
 import { LoginComponent } from './login-fr/login-fr.component';
+import { LoginEnComponent } from './login-en/login-en.component';
+import { LoginArComponent } from './login-ar/login-ar.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -7,8 +9,11 @@ import { FormulaireComponent } from '../app/formulaire/formulaire.component'
 const routes: Routes = [
 
   { path:'',component: HomeComponent},
-  { path:'login',component: LoginComponent},
+  { path:'login-fr',component: LoginComponent},
+  { path:'login-en',component: LoginEnComponent},
+  { path:'login-ar',component: LoginArComponent},
   { path:'formulaire',component: FormulaireComponent},
+
 ];
 
 @NgModule({
